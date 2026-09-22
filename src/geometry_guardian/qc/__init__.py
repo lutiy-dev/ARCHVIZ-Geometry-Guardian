21f8a0797088bdc45261c8f7ac2b02dfe9593d2f
OVizLAB <67496316+lutiy-dev@users.noreply.github.com>
2026-09-22 08:00:20 +0300

    Add src/geometry_guardian/qc/__init__.py

---
+++ src/geometry_guardian/qc/__init__.py
@@ -0,0 +1,4 @@
+from .coverage import CoverageSummary, facade_status, summarize_statuses
+from .statuses import QCStatus
+
+__all__ = ["CoverageSummary", "facade_status", "summarize_statuses", "QCStatus"]
