95b7703fe702eb706aa87f6b93ada00ca786510a
OVizLAB <67496316+lutiy-dev@users.noreply.github.com>
2026-09-22 08:00:15 +0300

    Add src/geometry_guardian/reference/__init__.py

---
+++ src/geometry_guardian/reference/__init__.py
@@ -0,0 +1,25 @@
+from .passport import (
+    CapabilityState,
+    PropertyEvidence,
+    ReferenceElement,
+    ReferencePassport,
+    VerificationState,
+)
+from .scene_truth import (
+    SceneReferenceEvidence,
+    SceneReferenceStatus,
+    SceneReferenceValidation,
+    validate_scene_reference,
+)
+
+__all__ = [
+    "CapabilityState",
+    "PropertyEvidence",
+    "ReferenceElement",
+    "ReferencePassport",
+    "VerificationState",
+    "SceneReferenceEvidence",
+    "SceneReferenceStatus",
+    "SceneReferenceValidation",
+    "validate_scene_reference",
+]
